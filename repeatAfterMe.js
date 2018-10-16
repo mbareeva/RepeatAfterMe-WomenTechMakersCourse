985a3363a1f94c233592094455e03ba31224fada
Mayya Bareeva <beeemaja@gmail.com>
2018-10-16 10:40:22 +0200

    done

--- repeatAfterMe.js
+++ repeatAfterMe.js
@@ -5,7 +5,12 @@ With the help of experience of other users, you do not have to worry about
 making up a plan of a trip. You can just look up the post with map and locations
 on it, reviews of visited places, save the post and repeat the experience.
 
-Four classes: User, Post, Map, Location.
+Four classes: User, Post, Map, Location.#
+
+User can post Post(s)
+Post should contain Map
+Map has Locations on it
+
 */
 
 var User = class {
@@ -14,26 +19,37 @@ constructor(name, email){
   this.email = email;
   this.posts = [];
 }
-    sayName(){
-      console.log("Hello, " + this.name! "Share with us your last trip!")
-  }
 
+    sayName(){
+      console.log("Hello, " + this.name + "!" + " Share with us your last trip!")
+  }
     sayEmail(){
       return this.email;
     }
-
     makePost(post){
         this.posts.push(post)
     }
 }
 
+
+
 var Post = class {
       constructor(topic, description){
         this.topic = topic
         this.description = description
-        this.map = ...
+        this.maps = []
       }
-    }
+
+      writeTopic(){
+        console.log("The topic is " + this.topic)
+      }
+      describeTrip(){
+        console.log(this.description)
+      }
+      addMapInPost(map){
+        this.maps.push(map)
+      }
+  }
 
 var leo = new User ("Leo", "leonardo@gmail.com")
 var christine = new User ("Christine", "christ92@hotmail.com")
@@ -44,18 +60,62 @@ var post3 = new Post("Me as Au-Pair in France", "My year of living with host fam
 leo.makePost(post1)
 christine.makePost(post2)
 phil.makePost(post3)
+post1.addMapInPost()
+post2.addMapInPost()
+post3.addMapInPost()
+
+// test
+leo.sayName()
+christine.sayEmail()
 
 var Map = class {
       constructor(){
         this.locations = []
       }
+
+      addLocOnMap(locationN){
+        this.locations.push(locationN)
+      }
 }
 
 var Location = class {
-    constructor(placeName, placeAddress){
+    constructor(placeName, placeAddress, rating){
         this.placeName = placeName
         this.placeAddress = placeAddress
+        this.rating = rating
       }
-    }
 
-}
+      sayPlaceName(){
+        return this.placeName
+      }
+      sayPlaceAddress(){
+        return this.placeAddress
+      }
+      sayRating(){
+        return this.rating
+      }
+  }
+
+var accomodation = new Location("Alessandro Palace Hostel & Bar", "via Vicenza, 42", 4)
+var cafe = new Location("Pizzeria Del Secolo Roma", "via Palestro, 62",  3)
+var accomodation2 = new Location ("Private House", "Rue Saint-Maurice", 5)
+var cafe2 = new Location ("L'Atelier Gourmand", "2 Rue Saint-MAurice", 3)
+var accomodation3 = new Location("Pro Surf School Bali", "Pantai Kuta 33, Legian, Kuta", 5)
+var cafe3 = new Location ("Warung Damar", "Jalan Kartika Plaza", 4)
+
+var map = new Map()
+var map2 = new Map()
+var map3 = new Map()
+
+map.addLocOnMap(accomodation)
+map.addLocOnMap(cafe)
+map2.addLocOnMap(accomodation2)
+map2.addLocOnMap(cafe2)
+map3.addLocOnMap(accomodation3)
+map3.addLocOnMap(cafe3)
+
+//test
+console.log(accomodation)
+console.log(map)
+
+console.log(phil)
