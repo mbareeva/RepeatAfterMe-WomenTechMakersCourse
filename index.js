d7a3baf8ba36c2be54a045a9ff1b10c6f0e6726d
Mayya Bareeva <beeemaja@gmail.com>
2018-11-05 01:23:09 +0100

    post and delete to be added

--- index.js
+++ index.js
@@ -11,84 +11,30 @@ User can post Post(s)
 Post should contain Map
 Map has Locations on it
 */
+const express = require('express')
+const UserService = require('./services/user-service')
 
 
-const User = require("./models/user")
-const Location = require("./models/location")
-const Map = require("./models/map")
-const Post = require("./models/post")
-const Chalk = require("chalk")
-const UserService = require("./services/user-service")
+const app = express()
+app.set('view engine', 'pug')
 
-async function main(){
+app.get('/', async (req, res) => {//response
+    res.render('index', {contributors})
+})
 
-/*
-Create a user
-*/
-const leo = new User ("Leo", "leonardo@gmail.com")
-const christine = new User ("Christine", "christ92@hotmail.com")
-const phil = new User ("Philipp", "phil_white@mail.ru")
+app.get('/person/all', async (req, res) => {
+    const contributors = await UserService.findAll()
+    res.render('index', {contributors})
+})
 
-/*
-Set a post
-*/
-const post1 = new Post("2 days in Rome", "Brief overlook of worthy places in Rome")
-const post2 = new Post("Surfing camp in Bali", "How I learned serfing and travelled around suburbs")
-const post3 = new Post("Me as Au-Pair in France", "My year of living with host family")
+app.get('/person/:id', async (req, res) => {
+    res.send(await UserService.find(req.params.id))
+})
 
-/*
-Setting up locations
-*/
-const accomodation = new Location("Alessandro Palace Hostel & Bar", "via Vicenza, 42", 4)
-const cafe = new Location("Pizzeria Del Secolo Roma", "via Palestro, 62",  3)
-const accomodation2 = new Location ("Private House", "Rue Saint-Maurice", 5)
-const cafe2 = new Location ("L'Atelier Gourmand", "2 Rue Saint-MAurice", 3)
-const accomodation3 = new Location("Pro Surf School Bali", "Pantai Kuta 33, Legian, Kuta", 5)
-const cafe3 = new Location ("Warung Damar", "Jalan Kartika Plaza", 4)
-//test
-//console.log(accomodation)
+//app.post('/person', async (req, res) => {
+//        console.log(req.body)
+//})
 
-/*
-Create a map for each user
-*/
-const map = new Map("my route in Rome")
-const map2 = new Map("my trip to France")
-const map3 = new Map("Surroundings")
-
-/*
-Add visited locations on map
-*/
-map.addLocOnMap(accomodation)
-map.addLocOnMap(cafe)
-map2.addLocOnMap(accomodation2)
-map2.addLocOnMap(cafe2)
-map3.addLocOnMap(accomodation3)
-map3.addLocOnMap(cafe3)
-
-/*
-Add map(s) in post of a user
-*/
-post1.addMapInPost(map)
-post2.addMapInPost(map2)
-post3.addMapInPost(map3)
-
-//test
-//console.log(post1)
-
-/*
-Set a post for the user
-*/
-leo.makePost(post1)
-christine.makePost(post2)
-phil.makePost(post3)
-
-
-await UserService.add(leo)
-await UserService.add(christine)
-await UserService.add(phil)
-const people = await UserService.findAll()
-
-console.log(people[0].name)
-}
-
-main()
+app.listen(3000, () => {//request
+    console.log('Server is listening')
+})
